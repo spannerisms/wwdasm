2a433b6914a8392994c65ce9a7f7aedd1dc1b4fa
spannerisms <32842036+spannerisms@users.noreply.github.com>
2023-04-28 06:55:19 -0400

    BREAKFAST!
    Hello!

---
+++ symbols_sram.asm
@@ -0,0 +1,8 @@
+; developer initials used as a magic word
+DEVY            = $700000
+DEVS            = $700001
+DEVK            = $700002
+
+BESTRND         = $700063
+
+RAND            = $700068
---
+++ symbols_wram.asm
@@ -0,0 +1,37 @@
+INIDISPQ        = $7E006A
+NMITIMENQ       = $7E0097
+HDMAENQ         = $7E009C
+MDMAENQ         = $7E009E
+HDMAENNEW       = $7E009F
+
+; indexer for $0140
+THING           = $7E0200
+
+; game mode?
+; 00 - round
+; 01 - lesson
+; 81 - vs COM
+MODE            = $7E0691
+
+ROUND           = $7E0699
+
+BOARD           = $7E0820
+
+; time left until wario/birdo phases
+BTIME           = $7E0A20
+
+; columns with free space
+FREECCT         = $7E0A34
+FREECOL         = $7E0A35
+
+
+; count of color on the board
+COLORS          = $7E0A5B
+
+; list of available bombs
+CLIST           = $7E0A5C
+
+; opponent ID
+COMVS           = $7E1373
+
+P1POSE          = $7E140C
